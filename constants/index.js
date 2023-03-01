797fc0ae0258e5abe7d9b65e240145345b075dbb
MattiaGo <91184227+MattiaGo@users.noreply.github.com>
2023-03-01 18:21:33 +0100

    Update

--- constants/index.js
+++
@@ -1,9 +0,0 @@
-const contractAddresses = require("./contractAddresses.json")
-const abi = require("./abi.json")
-const bytecode = require("./bytecode.json")
-
-module.exports = {
-    contractAddresses,
-    abi,
-    bytecode,
-}
